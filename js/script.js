307b77c91c889b75d67f065a70127f45ba36d821
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2023-01-30 20:46:13 +0600

    testimonial done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.test_main').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        
+      });
+})
