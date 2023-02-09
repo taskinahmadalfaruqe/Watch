89eb9581cd43c1db79bc65ad006c2c2885bd5826
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2023-02-10 01:33:59 +0600

    project Done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(function(){
+    "use strict";
+    let menu = document.querySelector(".nav_item");
+    let bars = document.querySelector(".bars");
+    
+    bars.addEventListener("click", ()=>{
+        menu.classList.toggle("active")
+    });
+
+
     $('.test_main').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -9,5 +18,37 @@ $(function(){
         focusOnSelect:true,
         centerMode: true,
         centerPadding: '0px',
+        responsive: [
+            {
+              breakpoint: 1199.98,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 991.98,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 767.98,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 575.98,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
 })
